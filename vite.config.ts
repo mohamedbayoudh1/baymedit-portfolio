a048ac94e30734b0ff1415eaa920b182901b0967
Mohamed Bayoudh <61636436+mohamedbayoudh1@users.noreply.github.com>
2025-12-11 12:06:21 +0100

    lastvf

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // <-- important for Netlify deployment
   server: {
     host: "::",
     port: 8080,
